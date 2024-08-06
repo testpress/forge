0a0066958d4c98c7af0906947b946649cbba7433
Bharathwaaj S <bharath@testpress.in>
2024-08-06 10:11:56 +0000

    fix: Fix app path in settings

--- {{cookiecutter.project_slug}}/config/base.py
+++ {{cookiecutter.project_slug}}/config/base.py
@@ -33,7 +33,7 @@ DEBUG = env.bool("DJANGO_DEBUG", False)
 
 # fmt: off
 LOCAL_APPS = [
-    "apps.app.{{ cookiecutter.project_slug.replace(" ", " ")|capitalize }}Config",  # noqa: E501
+    "app.apps.{{ cookiecutter.project_slug.replace(" ", " ")|capitalize }}Config",  # noqa: E501
 ]
 # fmt: on
 
