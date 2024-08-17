ca390a689f93081ae00731d9a6447896faa39547
Bharathwaaj S <bharath@testpress.in>
2024-08-17 12:56:48 +0000

    feat: Add INTERNAL_IPS configuration for exposing debug context variable
    - Added `INTERNAL_IPS` to `local.py` to include `127.0.0.1` and `localhost`.
    - This is essential for ensuring the `debug` context variable is available in templates during development, as it relies on the client’s IP being listed in `INTERNAL_IPS`.
    - Without this configuration, the `debug` context variable wouldn’t be exposed, affecting the ability to see debug information in development.
    
    This update ensures a smooth local development experience by making debug information accessible as intended.

--- {{cookiecutter.project_slug}}/config/local.py
+++ {{cookiecutter.project_slug}}/config/local.py
@@ -5,3 +5,10 @@ DEBUG = True
 
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=["*"])  # noqa: F405
+
+# config/settings/local.py
+
+INTERNAL_IPS = [
+    "127.0.0.1",
+    "localhost",
+]
