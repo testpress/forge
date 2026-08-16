598d7d3817c016741377a0243b2176858c5188ac
Bharath <bharath@testpress.in>
2026-08-16 21:42:15 +0530

    test: DO NOT MERGE - verify the mypy gate fails CI

--- {{cookiecutter.project_slug}}/app/models/permission.py
+++ {{cookiecutter.project_slug}}/app/models/permission.py
@@ -6,4 +6,4 @@ class Permission(models.Model):
     name = models.CharField(max_length=255)
 
     def __str__(self) -> str:
-        return self.name
+        return 12345  # DELIBERATE TYPE ERROR - verification only
